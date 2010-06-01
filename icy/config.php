14f56b38f618e6f5fb02b2e9c7c70d5752e3a06b
Mattenrone <Mattenrone@b0c4deed-faa8-ce91-1e61-3a95bb203d72>
2010-06-01 19:11:21 +0000

    First real commit. Added: #Folders #auth.class.php #config.php #db.class.php

---
+++ icy/config.php
@@ -0,0 +1,18 @@
+<?php defined('SYSINIT') or die('No direct access');
+/*********************************************
+Configuration for ICY CMS.
+
+CONFIGURE THE DATABASE IN THE db.lib.php FILE
+FOUND IN icy/lib/ FOLDER
+
+
+
+The base url of the directory containing the
+system folder. WITH a trailing slash.
+Eg.
+$config['baseurl'] = "http://www.example.com/";
+*********************************************/
+$config['baseurl'] = "http://localhost/us2/ice-cms/";
+
+
+?>
